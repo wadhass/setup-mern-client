7e6e9ba315d38a2f0f33c0f97b1256aedd6f36fb
wadhass <ahmedhassansumu@gmail.com>
2025-03-24 13:55:51 +0200

    update-mern-client

--- src/home/BannerCard.jsx
+++ src/home/BannerCard.jsx
@@ -19,11 +19,11 @@ const BannerCard = () => {
         modules={[EffectCards]}
         className="mySwiper"
         >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
+            <SwiperSlide><img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1721918653l/198902277.jpg" alt="" /></SwiperSlide>
+            <SwiperSlide><img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1689843639l/123211531._SY475_.jpg" alt="" /></SwiperSlide>
+            <SwiperSlide><img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1738170651l/199347538._SY475_.jpg" alt="" /></SwiperSlide>
+            <SwiperSlide><img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1705935887l/201751300._SY475_.jpg" alt="" /></SwiperSlide>
+            <SwiperSlide><img src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1695915526l/125045190._SY475_.jpg" alt="" /></SwiperSlide>
         </Swiper>
     </div>
   )
